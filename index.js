14647c4a0916751af21e5db086e61748d6199875
Lewis <lokiyule@gmail.com>
2025-03-22 12:35:22 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+function createEmployeeRecords(arrays) {
+    return arrays.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(employee, dateTime) {
+    let [date, hour] = dateTime.split(" ");
+    employee.timeInEvents.push({ type: "TimeIn", date, hour: parseInt(hour) });
+    return employee;
+}
+
+function createTimeOutEvent(employee, dateTime) {
+    let [date, hour] = dateTime.split(" ");
+    employee.timeOutEvents.push({ type: "TimeOut", date, hour: parseInt(hour) });
+    return employee;
+}
+
+function hoursWorkedOnDate(employee, date) {
+    let inEvent = employee.timeInEvents.find(event => event.date === date);
+    let outEvent = employee.timeOutEvents.find(event => event.date === date);
+    return (outEvent.hour - inEvent.hour) / 100;
+}
+
+function wagesEarnedOnDate(employee, date) {
+    return hoursWorkedOnDate(employee, date) * employee.payPerHour;
+}
+
+function allWagesFor(employee) {
+    return employee.timeInEvents.reduce((total, event) => total + wagesEarnedOnDate(employee, event.date), 0);
+}
+
+function calculatePayroll(employees) {
+    return employees.reduce((total, employee) => total + allWagesFor(employee), 0);
+}
